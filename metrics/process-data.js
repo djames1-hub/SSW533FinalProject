5f6910f3aaca93703f46c57fa456519d10d686fe
Dylan James <djjames455@gmail.com>
2022-05-09 20:50:31 -0400

    Add aeshell metrics

--- metrics/process-data.js
+++ metrics/process-data.js
@@ -109,7 +109,7 @@ const storeMetricsInCSVFile = (owner, repo, metrics) => {
 
             console.log('Next date:', getNextYear(firstCommit.startDate));
 
-            const MAX_INTERVAL_COMMITS = 10;
+            const MAX_INTERVAL_COMMITS = 6;
             let nextYear = null;
             let commits = [];
             let recentCommit = null;
